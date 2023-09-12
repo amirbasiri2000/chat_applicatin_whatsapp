9136748e34e8e27f6622df438aa78716f33cacad
amirbasiri2000 <basiriamir2000@gmail.com>
2023-09-12 17:12:55 +0330

    second

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let menuFlag = false;
 
 let messages = [];
 
+let receivedMsg = `<div class="received__message__content">
+                    <div class="receive__message">
+                    <span class="receive__message-user">Amir Basiri</span>
+                    <p class="receive__message-msg">این یک پیام تستی است</p>
+                    <span class="receive__message-date">12, sep, 2023</span>
+                  </div></div>`;
+
 // ____________________________________
 
 // ____________________---___Event Listener
@@ -40,11 +47,12 @@ function sentMsg(e) {
     currentdate.getHours() +
     ":" +
     currentdate.getMinutes();
-  let st = ` <div class="sent__message">
-              <span class="sent__message-user">Amir Basiri</span>
+  let st = ` <div class="sent__message__content">
+  <div class="sent__message">
+              <span class="sent__message-user">You</span>
               <p class="sent__message-msg"> ${textInput.value}</p>
               <span class="sent__message-date">${datetime}</span>
-            </div> `;
+            </div> </div>`;
   if (textInput.value) {
     chatMsg.insertAdjacentHTML("beforeend", st);
   }
